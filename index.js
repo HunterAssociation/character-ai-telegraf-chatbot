e34d09a4d190992beef6ccbedc79e4024e1a0ce6
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-05-02 22:45:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Telegraf = require('telegraf');
-const bot = new Telegraf(''); //bot token
+const bot = new Telegraf('6259424194:AAG800ZxhIHYwRZLk2cJ23dUaqaC6XbZkjA'); //bot token
 const { read } = require('fs');
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
 
 (async() => {
-    await characterAI.authenticateWithToken(''); //account token or using guest authenticateAsGuest() 
-    const characterId = "" //characterid token
+    await characterAI.authenticateWithToken('711a8264e246a6eaf70be0f334862cef57009c6b'); //account token or using guest authenticateAsGuest() 
+    const characterId = "8ehNKpVN7t4foQzUY0GRw7DIfzrsXe1ahXc_ajL-W6I" //characterid token
     const chat = await characterAI.createOrContinueChat(characterId);
     
     bot.start((ctx) => {
